7c54f5a7dedcd249fcc977f04c08212b23fb00c3
Yani <yani1229833@gmail.com>
2025-10-28 22:11:59 +0000

    Update main.py

--- app/main.py
+++ app/main.py
@@ -4,12 +4,10 @@ from nav import home, dashboard, notes, journal
 pages = {"Home": home, "Dashboard": dashboard, "Notes": notes, "Journal": journal}
 
 def main():
-    if "log" not in st.session_state:
-        st.session_state["log"] = False
-    if not st.session_state["log"]:
+    if "auth" not in st.session_state:
+        st.session_state["auth"] = ""
+    if st.session_state["auth"] != st.secrets["PASSWORD"]:
         st.text_input("This is a personal app. Please enter your password to continue:", type="password", key="auth")
-        if st.session_state["auth"] == st.secrets["PASSWORD"]:
-            st.session_state["log"] = True
     else:
         page = st.sidebar.radio("NIMOZINI", list(pages.keys()))
         pages[page].show_page()
@@ -17,4 +15,5 @@ def main():
 if __name__ == "__main__":
 
     main()
+
 
