0fa9c242493a4a496773924e6358c9140e181edb
Yani <yani1229833@gmail.com>
2025-10-28 21:55:25 +0000

    Update main.py
    Add session state management for user login.

--- app/main.py
+++ app/main.py
@@ -4,13 +4,15 @@ from nav import home, dashboard, notes, journal
 pages = {"Home": home, "Dashboard": dashboard, "Notes": notes, "Journal": journal}
 
 def main():
-    p = st.empty()
-    p = st.text_input("This is a personal app. Please enter your password to continue:", type="password", key="auth")
-    if st.session_state["auth"] != st.secrets["PASSWORD"]:
-        st.stop()
-    p = st.write(" ")
-    page = st.sidebar.radio("NIMOZINI", list(pages.keys()))
-    pages[page].show_page()
+    if "log" not in st.session_state:
+        st.session_state["log"] = False
+        st.text_input("This is a personal app. Please enter your password to continue:", type="password", key="auth")
+        if st.session_state["auth"] == st.secrets["PASSWORD"]:
+            st.session_state["log"] = True
+    if st.session_state["log"]:
+        page = st.sidebar.radio("NIMOZINI", list(pages.keys()))
+        pages[page].show_page()
 
 if __name__ == "__main__":
+
     main()
