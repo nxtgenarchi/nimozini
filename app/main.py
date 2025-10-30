c069f4858bc10083f1acdd96eb680e8b995f39bf
Yani <yani1229833@gmail.com>
2025-10-30 23:05:28 +0000

    Update main.py
    Added a placeholder for the authentication input field to improve user experience.

--- app/main.py
+++ app/main.py
@@ -6,9 +6,9 @@ pages = {"Home": home, "Dashboard": dashboard, "Notes": notes, "Journal": journa
 def main():
     if "auth" not in st.session_state:
         st.session_state["is_auth"] = False
-    
+    placeholder = st.empty()
     if not st.session_state["is_auth"]:
-        auth_value = st.text_input("This is a personal app. Please enter the correct password to continue:", type="password", key="auth_input")
+        auth_value = placeholder.text_input("This is a personal app. Please enter the correct password to continue:", type="password", key="auth_input")
         if auth_value:
             if auth_value == st.secrets.get("PASSWORD", ""):
                 st.session_state["is_auth"] = True
@@ -18,12 +18,14 @@ def main():
         else:
             st.info("Please enter the app password.")
             return
+    placeholder.write("")
     page = st.sidebar.radio("NIMOZINI", list(pages.keys()))
     pages[page].show_page()
 
 if __name__ == "__main__":
 
     main()
+
 
 
 
