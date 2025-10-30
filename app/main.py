aa1819fe0bca22f6f1c7050f2e61d6d0e2e926a8
Yani <yani1229833@gmail.com>
2025-10-30 22:53:56 +0000

    Update main.py
    Added authentication check to the main function, requiring a password to access the app.

--- app/main.py
+++ app/main.py
@@ -4,12 +4,29 @@ from nav import home, dashboard, notes, journal
 pages = {"Home": home, "Dashboard": dashboard, "Notes": notes, "Journal": journal}
 
 def main():
+    if "auth" not in st.session_state:
+        st.session_state["is_auth"] = False
+    
+    if not st.session_state["is_auth"]:
+        auth_value = st.text_input("This is a personal app. Please enter the correct password to continue:", type="password", key="auth_input")
+        if auth_value:
+            if auth_value == st.secrets.get("PASSWORD", ""):
+                st.session_state["is_auth"] = True
+                st.session_state.pop("auth_input", None)
+                st.experimental_rerun()
+            else:
+                st.error("Incorrect password.")
+                return
+        else:
+            st.info("Please enter the app password.")
+            return
     page = st.sidebar.radio("NIMOZINI", list(pages.keys()))
     pages[page].show_page()
 
 if __name__ == "__main__":
 
     main()
+
 
 
 
