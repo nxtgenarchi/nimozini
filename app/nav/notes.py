f46668809143c558abea7f3439e073c252f39fb0
Yani <yani1229833@gmail.com>
2026-03-20 20:24:09 +0000

    t46

--- app/nav/notes.py
+++ app/nav/notes.py
@@ -643,7 +643,7 @@ def note_editor():
     st.sidebar.title("Notes Toolbar")
     chosen_type = st.sidebar.selectbox("Add Block", list(["-"] + list(TYPES.keys())), key="chosen_type")
     if chosen_type and chosen_type != "-":
-        content = serialize(chosen_type,eval(TYPES[chosen_type])())
+        content = chosen_type,eval(TYPES[chosen_type])()
         if st.sidebar.button("Confirm Block Addition"):
             st.session_state["add_block"] = True
         if st.session_state.get("add_block"):
