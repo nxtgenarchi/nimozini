85840f4fae32f92a6838b85edbdefcb04e4db563
Yani <yani1229833@gmail.com>
2026-03-28 22:12:07 +0000

    t98

--- app/nav/home.py
+++ app/nav/home.py
@@ -2,4 +2,4 @@ import streamlit as st
 
 def show_page():
     st.title("NIMOZINI: Study & Mental Health Buddy")
-    st.image(https://zbykzsflapwijjqekuhx.supabase.co/storage/v1/object/public/fbkt/uploads/1774735261_Design%20sans%20titre.png, caption="Mnemosyne, Greek goddess of memory, symbolizes retaining knowledge and managing mental health effectively.")
+    st.image(st.secrets["SUPABASE_URL"] + "/storage/v1/object/public/fbkt/uploads/1774735261_Design%20sans%20titre.png", caption="Mnemosyne, Greek goddess of memory, symbolizes retaining knowledge and managing mental health effectively.")
